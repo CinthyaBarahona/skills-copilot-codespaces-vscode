df91b6cf114f14d0088b283dfd9577738bbd6417
Cinthya Barahona <noemybarahona24@gmail.com>
2023-07-17 19:47:04 -0600

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember(){
+    console.log('skillsMember');
+    var skills = document.getElementById('skills');
+    skills.style.display = 'block';
+    var member = document.getElementById('member');
+    member.style.display = 'none';
+}
